3d39e435bf132e962157cb12bb7908fb10a793ee
Jeff <d8amonk@users.noreply.github.com>
2015-06-06 15:07:51 -0600

    Update true_vs_predicted.R
    put some gamma on it

--- true_vs_predicted.R
+++ true_vs_predicted.R
@@ -1,5 +1,5 @@
-x_1 <- rnorm(1000,5,15)
-x_2 <- rnorm(1000,5,1)
+x_1 <- rnorm(1000,5,1)
+x_2 <- rnorm(1000,20,3)
 x_3 <- x_2*x_1
 
 hist(x_1, col = 'grey')
@@ -20,8 +20,8 @@ plot(x_1, y_1, pch = 20, col = 'red')
 run1 <- lm(y_1 ~ x_1 + x_2 + x_3)
 run1
 
-x_4 <- rgamma(1000,5,15)
-x_5 <- rgamma(1000,5,1)
+x_4 <- rgamma(1000,5,1)
+x_5 <- rgamma(1000,20,3)
 x_6 <- x_4^2
 
 true_error2 <- rnorm(1000,0,3)
